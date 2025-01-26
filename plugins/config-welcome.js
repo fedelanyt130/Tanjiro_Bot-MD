6de83f1d75a50544539a0686e3a5a354e06b79a5
Ian.xyz <taiwam34@gmail.com>
2025-01-26 14:52:06 -0300

    FLS

--- plugins/config-welcome.js
+++ plugins/config-welcome.js
@@ -3,10 +3,24 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
+    let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => welcome)
+    let pp2 = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => adios)
+  let img = await (await fetch(`${pp}`)).buffer()
+  let img2 = await (await fetch(`${pp2}`)).buffer()
+
   let chat = global.db.data.chats[m.chat]
 
  if (chat.welcome && m.messageStubType == 27) {
-    let welcome = `*Hola ✨ @${m.messageStubParameters[0].split`@`[0]}*\n\n${global.welcome}`
-await conn.sendMini(m.chat, redes, dev, welcome, image, image, redeshost)
+    let welcome = ` 🍃 TANJIRO-AI \n「 Bienvenido 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Bienvenido/a 」\n「 ${groupMetadata.subject} 」\n\n`
+await conn.sendMini(m.chat, botname, dev, welcome, img, img, fkontak)
   }
-}
+
+  if (chat.welcome && m.messageStubType == 28) {
+    let bye = ` 🍃 TANJIRO-AI \n「 Adios 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Sҽ ϝυҽ 」\n「 Nunca te quisimos aqui 」\n\n`
+await conn.sendMini(m.chat, botname, dev, bye, img, img, fkontak)
+  }
+
+  if (chat.welcome && m.messageStubType == 32) {
+    let kick = ` 🍃 TANJIRO-AI \n「 Adios 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Sҽ ϝυҽ 」\n「 Nunca te quisimos aqui 」\n\n
+await conn.sendMini(m.chat, botname, dev, kick, img, img, fkontak)
+}}
