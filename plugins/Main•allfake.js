7fe45dc5578480e294ea4cafc4db68e3f16a55f7
Ian.xyz <taiwam34@gmail.com>
2025-01-26 14:59:23 -0300

    FLS

--- plugins/Main•allfake.js
+++ plugins/Main•allfake.js
@@ -74,7 +74,7 @@ global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo
 }}, { quoted: m }
 
 global.icono = [ 
-'https://qu.ax/zNqAj.jpg'
+'https://qu.ax/ECoeX.jpeg'
 ].getRandom()
 
 global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363392482966489@newsletter", serverMessageId: 100, newsletterName: namechannel, }, externalAdReply: { showAdAttribution: true, title: botname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
