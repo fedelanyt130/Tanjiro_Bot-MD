71341c989d64078d08db9ead710a1f450ca1530f
fedelanyt130 <federicoyt150@gmail.com>
2025-06-26 12:49:13 -0300

    Update rg-verificar.js

--- plugins/rg-verificar.js
+++ plugins/rg-verificar.js
@@ -1,60 +1,106 @@
-import db from '../lib/database.js'
-import fs from 'fs'
-import PhoneNumber from 'awesome-phonenumber'
+import axios from 'axios'
 import { createHash } from 'crypto'
+import PhoneNumber from 'awesome-phonenumber'
+import moment from 'moment-timezone'
 
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+let handler = async function (m, { conn, text, args, usedPrefix, command }) {
+    let user = global.db.data.users[m.sender]
+    let name2 = conn.getName(m.sender)
+    let whe = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
+    let perfil = await conn.profilePictureUrl(whe, 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
 
-let handler = async function (m, { conn, text, usedPrefix, command }) {
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let mentionedJid = [who]
-  let bio = 0, fechaBio
-  let sinDefinir = 'Es privada'
-  let biografia = await conn.fetchStatus(m.sender).catch(() => null)
-  if (!biografia || !biografia[0] || biografia[0].status === null) {
-    bio = sinDefinir
-    fechaBio = "Fecha no disponible"
-  } else {
-    bio = biografia[0].status || sinDefinir
-    fechaBio = biografia[0].setAt ? new Date(biografia[0].setAt).toLocaleDateString("es-ES", { day: "2-digit", month: "2-digit", year: "numeric" }) : "Fecha no disponible"
-  }
+    if (user.registered === true) {
+        return m.reply(`《💖》𝗬𝗮 𝘁𝗲 𝗲𝗻𝗰𝘂𝗲𝗻𝘁𝗿𝗮𝘀 𝗿𝗲𝗴𝗶𝘀𝘁𝗿𝗮𝗱𝗼.\n\n¿𝗤𝘂𝗶𝗲𝗿𝗲 𝘃𝗼𝗹𝘃𝗲𝗿 𝗮 𝗿𝗲𝗴𝗶𝘀𝘁𝗿𝗮𝗿𝘀𝗲?\n\n𝗨𝘀𝗲 𝗲𝘀𝘁𝗲 𝗰𝗼𝗺𝗮𝗻𝗱𝗼 𝗽𝗮𝗿𝗮 𝗲𝗹𝗶𝗺𝗶𝗻𝗮𝗿 𝘀𝘂 𝗿𝗲𝗴𝗶𝘀𝘁𝗿𝗼.\n*${usedPrefix}unreg*`)
+    }
+    
+    if (!Reg.test(text)) return m.reply(`《💖》Eʟ ғᴏʀᴍᴀᴛᴏ ɪɴɢʀᴇsᴀᴅᴏ ᴇs ɪɴᴄᴏʀʀᴇᴄᴛᴏ\n\nUsᴏ ᴅᴇʟ ᴄᴏᴍᴀɴᴅᴏ: ${usedPrefix + command} 𝗻𝗼𝗺𝗯𝗿𝗲.𝗲𝗱𝗮𝗱\nEᴊᴇᴍᴘʟᴏ : *${usedPrefix + command} ${name2}.14*`)
+    
+    let [_, name, splitter, age] = text.match(Reg)
+    if (!name) return m.reply('《💖》Eʟ ɴᴏʍ𝗯𝗿𝗲 ɴᴏ ᴘᴜᴇᴅᴇ ᴇsᴛᴀʀ ᴠᴀᴄɪᴏ.')
+    if (!age) return m.reply('《💖》Lᴀ ᴇᴅᴀᴅ ɴᴏ ᴘᴜᴇᴅᴇ ᴇsᴛᴀʀ ᴠᴀᴄɪ́ᴀ.')
+    if (name.length >= 100) return m.reply('《💖》El nombre es demasiado largo.')
+    
+    age = parseInt(age)
+    if (age > 1000) return m.reply('《💖》 *ʟᴀ ᴇᴅᴀᴅ ɪɴɢʀᴇsᴀᴅᴀ ᴇs ɪɴᴄᴏʀʀᴇᴄᴛᴀ*')
+    if (age < 5) return m.reply('《💖》 *ʟᴀ ᴇᴅᴀᴅ ɪɴɢʀᴇsᴀᴅᴀ ᴇs ɪɴᴄᴏʀʀᴇᴄᴛᴀ*')
+    
+    user.name = name.trim()
+    user.age = age
+    user.regTime = +new Date
+    user.registered = true
+    global.db.data.users[m.sender].money += 600
+    global.db.data.users[m.sender].estrellas += 10
+    global.db.data.users[m.sender].exp += 245
+    global.db.data.users[m.sender].joincount += 5    
 
-  let user = global.db.data.users[m.sender]
-  let name2 = conn.getName(m.sender)
-  if (user.registered === true) return m.reply(`🌙 *Ya te encuentras registrado*\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg*`)
-  if (!Reg.test(text)) return m.reply(`🌙 *Formato incorrecto*\n\n*Uso del comando: ${usedPrefix + command} nombre.edad*\n*Ejemplo: ${usedPrefix + command} ${name2}.20*`)
-  let [_, name, splitter, age] = text.match(Reg)
-  if (!name) return m.reply('🌙 *El nombre no puede estar vacío*')
-  if (!age) return m.reply('🌙 *La edad no puede estar vacía*')
-  if (name.length >= 100) return m.reply('🌙 *El nombre es demasiado largo*')
-  age = parseInt(age)
-  if (age > 1000) return m.reply('🌙 *El abuelo desea jugar al bot :D*')
-  if (age < 5) return m.reply('🌙 *No estan permitidos los niños :D*')
+    let who;
+    if (m.quoted && m.quoted.sender) {
+        who = m.quoted.sender;
+    } else {
+        who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+    }
+    
+    let sn = createHash('md5').update(m.sender).digest('hex')
+    let regbot = '🌙 𝗥 𝗘 𝗚 𝗜 𝗦 𝗧 𝗥 𝗔 𝗗 𝗢 🌙\n`
+    regbot += `────────\n`
+    regbot += `🌙 *Nombre:* ${name}\n`
+    regbot += `🌙 *Edad:* ${age} años\n`
+    regbot += '🌙 ${global.currency}* » 40\n
+    regbot += `🌙 *Experiencia* » 300\n`
+    regbot += `─────────\n`
+    regbot += `💖 usa *#menu* para los comandos.`
 
-  user.name = name + '✓'.trim()
-  user.age = age
-  user.descripcion = bio
-  user.regTime = +new Date
-  user.registered = true
-  global.db.data.users[m.sender].exp += 300
-
-  let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 20)
-  let regbot = `🍃 𝗥 𝗘 𝗚 𝗜 𝗦 𝗧 𝗥 𝗔 𝗗 𝗢 🍃\n`
-  regbot += `__________________\n`
-  regbot += `🌙 𝗡𝗼𝗺𝗯𝗿𝗲 » ${name}\n`
-  regbot += `🌙 𝗘𝗱𝗮𝗱 » ${age} años\n`
-  regbot += `__________________\n`
-  regbot += `   𝗥𝗲𝗰𝗼𝗺𝗽𝗲𝗻𝘀𝗮𝘀:\n`
-  regbot += `🌙 *${global.currency}* » 40\n`
-  regbot += `🌙 *Experiencia* » 300\n`
-  regbot += `__________________\n`
-
-  await m.react('📩')
   await conn.sendMessage(m.chat, {
-    image: { url: global.banner }, 
-    caption: regbot
-  }, { quoted: m })
-}
+        text: regbot,
+        contextInfo: {
+            externalAdReply: {
+                title: '💙 REGISTRADO•A 💙',
+                thumbnailUrl: 'https://files.catbox.moe/g8izji.jpg',
+                mediaType: 1,
+                renderLargerThumbnail: true
+            }
+        }
+    }, { quoted: m });
+
+/*    await m.react('📪')
+  await conn.sendMessage(m.chat, {
+           text: regbot, 
+        contextInfo: {
+            externalAdReply: {
+                showAdAttribution: true,                      
+                containsAutoReply: true,     
+                renderLargerThumbnail": true,
+                title: '💙 REGISTRADO•A 💙',  
+                body: dev,  
+                containsAutoReply: true,
+                showAdAttribution: true,
+                mediaType: 1, 
+                thumbnailUrl: 'https://files.catbox.moe/g8izji.jpg' }}}, {quoted: m})
+*/
+let chtxt = `👤 *𝚄𝚜𝚎𝚛* » ${m.pushName || 'Anónimo'}
+🗂 *𝚅𝚎𝚛𝚒𝚏𝚒𝚌𝚊𝚌𝚒𝚘́𝚗* » ${user.name}
+🍨 *𝙴𝚍𝚊𝚍* » ${user.age} años
+⌨️ *𝙳𝚎𝚜𝚌𝚛𝚒𝚙𝚌𝚒𝚘𝚗* » ${user.descripcion}
+🍭 *𝙽𝚞𝚖𝚎𝚛𝚘 𝚍𝚎 𝚛𝚎𝚐𝚒𝚜𝚝𝚛𝚘* »
+⤷ ${sn}`;
+
+    let channelID = '120363402097425674@newsletter';
+        await conn.sendMessage(channelID, {
+        text: chtxt,
+        contextInfo: {
+            externalAdReply: {
+                title: "【🔔 𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐂𝐈𝐎́𝐍 𝐃𝐄 𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐎 🔔】",
+                body: '🥳 ¡𝚄𝚗 𝚞𝚜𝚞𝚊𝚛𝚒𝚘 𝚗𝚞𝚎𝚟𝚘 𝚎𝚗 𝚖𝚒 𝚋𝚊𝚜𝚎 𝚍𝚎 𝚍𝚊𝚝𝚘𝚜!',
+                thumbnailUrl: perfil,
+                sourceUrl: redes,
+                mediaType: 1,
+                showAdAttribution: false,
+                renderLargerThumbnail: false
+            }
+        }
+    }, { quoted: null });
+};
 
 handler.help = ['reg']
 handler.tags = ['rg']
