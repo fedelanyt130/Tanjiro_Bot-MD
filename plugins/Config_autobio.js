0cdb0218c83ca96a57a1f6d77442415054ccaa64
fedelanyt130 <federicoyt150@gmail.com>
2025-06-26 22:26:05 -0300

    Update Config_autobio.js

--- plugins/Config_autobio.js
+++ plugins/Config_autobio.js
@@ -10,7 +10,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `${global.botname} 🌙 |「✨」Activo: ${uptime} | Dev: 𝖿ᥱძᥱ` 
+let bio = `${global.botname} 🌙 |「⚔」Activo: ${uptime} | Dev: 𝖿ᥱძᥱ` 
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 } }
