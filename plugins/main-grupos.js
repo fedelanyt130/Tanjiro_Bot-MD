fc3bcafd69263a6a72a3650b09dc3fcd77a86359
fedelanyt130 <federicoyt150@gmail.com>
2025-06-29 22:52:44 -0300

    Create main-grupos.js

---
+++ plugins/main-grupos.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+    let grupos = "*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad* ⭐\n\n" +
+                 "1- Grupo ofc\n" +
+                 "*✰* https://chat.whatsapp.com/FoVnxJ64gYV6EZcfNVQUfJ?mode=r_t" +
+                 "*─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*\n\n" +
+                 "➠ Enlace anulado? entre aquí! \n\n" +
+                 "⚔ Canal :\n" +
+                 "*✰*https://whatsapp.com/channel/0029VbAfd7zDDmFXm5adcF31" +
+                 "> By fede.xyz";
+
+    // Asegúrate de definir 'imagen2' correctamente antes de usarlo
+    let imagen2 = 'https://files.catbox.moe/1syk2a.jpg';
+
+    // Define los emojis que quieres usar
+    let emojis = '⚔';
+
+    await conn.sendFile(m.chat, imagen2, "ian.jpg", grupos, m, null, rcanal);
+    await m.react(emojis);
+}
+
+handler.help = ['grupos'];
+handler.tags = ['main'];
+handler.command = ['grupos', 'iangrupos', 'gruposian'];
+
+export default handler;
